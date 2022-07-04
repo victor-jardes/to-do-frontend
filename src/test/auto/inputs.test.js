24c08d6f0bf32294d90dfaef8a3c2ae4e586e0e7
victor jardes <victor_maanoel@outlook.com>
2022-07-03 22:02:07 -0400

    build(test): chage test

--- src/test/auto/inputs.test.js
+++ src/test/auto/inputs.test.js
@@ -1,36 +1,55 @@
 import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
 import App from '../../App';
+// import MainTasks from '../../components/MainTask';
 
 describe('Test inputs in screen', () => {
-  describe('Test input For Write', () => {
-
+  describe('1 - Test if buttons are being rederized', () => {
     it('should exists input for write task', () => {
       render(<App />);
 
-      const getInputForWriteTask = screen.getByLabelText("New Task:");
+      const getInputForWriteTask = screen.getByLabelText('New Task:');
 
       expect(getInputForWriteTask)
         .toBeInTheDocument();
-      expect(getInputForWriteTask.type).toEqual('text')
+      expect(getInputForWriteTask.type).toBe('text');
     });
 
-    //
-  });
-
-  describe('Test add task button', () => {
-
-    it('should exist input for add task in list', () => {
+    it('should exist input for add task', () => {
       render(<App />);
 
       const getInputeForAddTask = screen.getByTestId('button-add-task');
-      console.log(getInputeForAddTask.type)
 
       expect(getInputeForAddTask).toBeInTheDocument();
-      expect(getInputeForAddTask.type).toEqual('button')
+      expect(getInputeForAddTask.type).toBe('button');
+      expect(getInputeForAddTask).toHaveValue('adicionar');
     });
-
     //
   });
 
-})
+  // describe.only('2 - tests inputs functionality', () => {
+  //   it.skip('should can write, and after adding the value be empty', async () => {
+  //     const user = userEvent.setup();
+  //     render(<MainTasks />);
 
+  //     const getInputForWriteTask = screen.getByTestId('input-write-task');
+  //     const getInputeForAddTask = screen.getByTestId('button-add-task');
+
+  //     expect(getInputForWriteTask).toBeInTheDocument();
+  //     expect(getInputForWriteTask.type).toBe('text');
+  //     expect(getInputForWriteTask).toHaveValue('');
+
+  //     await user.type(getInputForWriteTask, 'tarefa nova');
+  //     expect(getInputForWriteTask).toHaveValue('tarefa nova');
+
+  //     expect(getInputeForAddTask).toBeInTheDocument();
+  //     expect(getInputeForAddTask.type).toBe('button');
+  //     expect(getInputeForAddTask).toHaveValue('adicionar');
+
+  //     await user.click(getInputeForAddTask);
+  //     console.log(getInputForWriteTask);
+
+  //     expect(getInputForWriteTask).toHaveValue('');
+  //   });
+});
