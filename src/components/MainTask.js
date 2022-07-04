71f78ae40288b399664142dbd4c504a1134941fc
victor jardes <victor_maanoel@outlook.com>
2022-07-03 22:21:16 -0400

    build(maintask.js): import and redering `List` component for show all tasks

--- src/components/MainTask.js
+++ src/components/MainTask.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddNewTask from './inputs/addTaskInputs/AddNewTask';
+import ListAllTasks from './list/ListAllTasks';
 
 export default function MainTask() {
   return (
     <div>
       <section>
         <AddNewTask />
+        <ListAllTasks />
       </section>
       <main />
     </div>
