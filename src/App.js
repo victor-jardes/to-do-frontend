d48f64c899b9523c5a5b808911786ed9522ccefb
victor jardes <victor_maanoel@outlook.com>
2022-07-04 22:35:49 -0400

    remove files and chage README.md

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
 import MainTask from './components/MainTask';
-// import ListAllTasks from './components/list/ListAllTasks';
 
 function App() {
   return (
     <div className="App">
       <h1>To-do</h1>
       <MainTask />
-      {/* <ListAllTasks /> */}
     </div>
   );
 }
